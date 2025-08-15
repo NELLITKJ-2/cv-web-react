76519743f98a4ce0a5800525833044f488a0195e
NELLITKJ-2 <nellievaniadestiantixtkj2@gmail.com>
2025-08-15 15:09:23 +0700

    nelli

--- src/modules/landing/section/HeroSection.tsx
+++ src/modules/landing/section/HeroSection.tsx
@@ -1,10 +1,11 @@
 import { Icon } from "@iconify/react";
 
-import iconNelli from "@assets/icons/profile/icon_yudha.png";
+import iconNelli from "@assets/icons/profile/icon_Nelli.png";
 
 export default function HeroSection() {
   const currentStatuses = [
-    { icon: "ri:code-s-slash-line", label: "Full Stack Developer" },
+    { icon: "ri:code-s-slash-line", label: "TKJ 2 student
+      " },
     { icon: "ri:map-pin-2-line", label: "Mojokerto, Jawa Timur" },
     {
       icon: "ri:linkedin-box-line",
@@ -16,8 +17,7 @@ export default function HeroSection() {
     <section className="flex lg:pt-16 pt-6" id="home">
       <div className="mx-auto text-center justify-content-center">
         <img
-          src={iconNelli}
-          className="w-20 mb-4 mx-auto"
+          src={iconNelli          className="w-20 mb-4 mx-auto"
           alt="icon-Nelli-with-mac"
         />
         <h1 className="text-3xl font-bold mb-4">I'm Nelli</h1>
